30ac72e030c377fc786a10bd0ffb22544e76b3cc
AlexeyBarkow <imbarkowal@gmail.com>
2016-04-23 14:37:39 +0300

    Realize children, css, data jQuery functions

--- custom-jquery/code.js
+++ custom-jquery/code.js
@@ -1,15 +1,15 @@
-
-
 function jQueryArray(array) {
     this.length = 0;
-    var that = this;
-    Array.prototype.forEach.call(array, (curr,i,arr) => {
-        that[i] = curr; 
-        that.length++;
-    });
+    // var that = this;
+    Array.prototype.push.apply(this, array);
+    // Array.prototype.forEach.call(array, (curr,i,arr) => {
+    //     that[i] = curr; 
+    //     that.length++;
+    // });
 	//make our jQuery array interpretate as array
     this.splice = function () {};
 }
+
 jQueryArray.prototype.addClass = function(classToAdd) {
     if (typeof classToAdd === 'string') {
         Array.prototype.forEach.call(this, (curr) => {
@@ -105,19 +105,83 @@ jQueryArray.prototype.attr = function(attributeName, value) {
     return this;
 }
 
-jQueryArray.prototype.children = function() {
 
+//ToDo: if one of selected elements metches the selector, its child will be added twice
+jQueryArray.prototype.children = function(selector) {
+    var res = new jQueryArray([]);
+    Array.prototype.forEach.call(this, (curr, index) => {
+        //ToDo: Change Array.push to my own concat method
+        Array.prototype.push.apply(res, $(selector === undefined ? "*" : selector, curr));
+    });
+    return res;
 }
 
-jQueryArray.prototype.css = function() {
-
+jQueryArray.prototype.css = function(propertyName, value) {
+    if (arguments.length == 1) {
+        if (this[0]) {
+            if (typeof propertyName === 'string') {
+                return this[0].style[propertyName];
+            } 
+            if (Array.isArray(propertyName)) {
+                var that = this;
+                return propertyName.map(curr => {
+                    return that[0].style[curr];
+                });
+            }
+            for (var style in propertyName) {
+                Array.prototype.forEach.call(this, curr => {
+                    curr.style[style] = propertyName[style];
+                });
+            }
+        }
+    } else {
+        if (typeof propertyName === 'string') {
+            if (typeof value === 'string' || typeof value === 'number') {
+                Array.prototype.forEach.call(this, curr => {
+                    curr.style[propertyName] = value;
+                });
+            };
+            if (typeof value === 'function') {
+                Array.prototype.forEach.call(this, (curr, index) => {
+                    curr.style[propertyName] = value(index, curr.style[propertyName]);
+                });
+            }
+        }
+    } 
+    return this;  
 }
 
-jQueryArray.prototype.data = function() {
+jQueryArray.prototype.data = function(key, value) {
+    if (this[0]) {
+        if (arguments.length === 0) {
+            return this[0].dataset;
+        }
+        if (arguments.length === 1 && typeof key === 'string') {
+            return this[0].dataset[key];
+        }
+    }
 
+    if (arguments.length === 1) {
+        for (var data in key) {
+            Array.prototype.forEach.call(this, curr => {
+                curr.dataset[data] = key[data];
+            }); 
+        }
+    }
+
+    if (arguments.length === 2) {
+        Array.prototype.forEach.call(this, curr => {
+            curr.dataset[key] = value;
+        });
+    }
+    return this;
 }
 
 jQueryArray.prototype.on = function() {
+
+}
+
+jQueryArray.prototype.one = function() {
 
 }
 
@@ -128,11 +192,10 @@ jQueryArray.prototype.each = function() {
 
 function $(selector, context) {
 	context = context || document;
-    if(!selector) {
-        return new jQueryArray([context]);
+    if (!selector) {
+        return new jQueryArray([]);
     }
     if (typeof selector === 'string') {
-  
         if (selector.trim()[0] == '<') {
             var div = context.createElement('div');
             div.innerHTML = selector;
