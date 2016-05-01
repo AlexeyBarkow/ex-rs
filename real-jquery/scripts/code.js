7b741953afe6720a838253061255ebc09a553374
AlexeyBarkow <imbarkowal@gmail.com>
2016-05-01 13:50:45 +0300

    Create edit button

--- real-jquery/scripts/code.js
+++ real-jquery/scripts/code.js
@@ -1,36 +1,52 @@
-$('.hide, .show').on('click', function () {
-	$('.add-form, #aside-hide, #aside-show').toggle();
+var   $submitButtonPressed
+	, $editingBookmark = null
+	;
+
+$('#hide-button').on('click', function () {
+	$('.add-form').toggle();
+	$(this).toggleClass('hide').toggleClass('show');
+		console.log($(this).data('text-original'), $(this).text())
+	if ($(this).text() === $(this).data('text-original')) {
+		$(this).text($(this).data('text-swap'));
+	} else {
+		if ($(this).text() === $(this).data('text-swap')) {
+			$(this).text($(this).data('text-original'));
+		}
+	}
 });
 
 
-// $('.inner-content > section').html($('.selected').data('text'));
 function toDefault() {
 	$('#index').val($('.bookmarks').children().length);
 	$('#title').val('');
 	$('#html-content').val('');	
 }
 
-// $('#title').val(function() {
-// 	return $(this).data('default');
-// });
+$('.bookmarks').children().on('click', onBookmarkClick);
+
 function onBookmarkClick () {
 	switchToBookmark($(this));
+	$('#index').val($(this).index());
+	$('#title').val($(this).children().text());
+	$('#html-content').val($(`.item:nth-child( ${ 1 + ( + $(this).index()) } )`).html().replace(/<!--[^>]*-->/g, ''));
 }
 
-$('.bookmarks').children().on('click', onBookmarkClick);
 
 function switchToBookmark($current) {
 	var $selected = $('.selected')
 		, index = $current.index();
+	$editingBookmark = $current;
 	$selected.toggleClass('selected');
-	console.log($(`.item:nth-child( ${ 1 + ( + $current.index()) } )`))
 	$(`.item:nth-child( ${ 1 + ( + $selected.index()) } )`).toggle();
 	$current.toggleClass('selected');
 	$(`.item:nth-child( ${ 1 + ( + $current.index()) } )`).toggle();
 }
 
-$('.add-form').submit(function () {
-	// console.log($('#index').val() === '');
+$('button[type=submit]').on('click', function () {
+	$submitButtonPressed = $(this);
+})
+
+$('.add-form').submit(function (e) {
 	var   isValid = true
 		, $index = $('#index')
 		, indexVal = $index.val()
@@ -58,7 +74,14 @@ $('.add-form').submit(function () {
 			, $innerContent = $('.inner-content')
 			;
 		$newBookmark.on('click', onBookmarkClick);
-		// console.log($index.val())
+		console.log($submitButtonPressed.attr('name'))
+		if ($submitButtonPressed.attr('name') === 'edit'){
+			console.log("le fu")
+			if ($editingBookmark) {
+				$(`.item:nth-child( ${ 1 + ( + $editingBookmark.index()) } )`).remove();
+				$editingBookmark.remove();
+			} 
+		}
 		if (indexVal === '0') {
 			$bookmarks.prepend($newBookmark);
 			$innerContent.prepend($newSection);
@@ -73,7 +96,10 @@ $('.add-form').submit(function () {
 		}
 		switchToBookmark($newBookmark);
 		toDefault();
+		//prevent page updating
+		return false;
 	}
 });
+
 
 toDefault();
