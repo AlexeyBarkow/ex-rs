d881ae0594668873e3b934f18465914ca5acaafc
AlexeyBarkow <imbarkowal@gmail.com>
2016-06-05 12:02:19 +0300

    Minor temp changes

--- lamernews/public/scripts/test-script.js
+++ lamernews/public/scripts/test-script.js
@@ -1,6 +1,14 @@
+require('../styles/test-style.css')
+
 function test() {
     let div = document.createElement('div');
     div.innerHTML = "<p>Me here</p>";
     return div.children[0];
 }
 module.exports = test;
+
+if(module.hot) {
+    module.hot.accept('../styles/test-style.css', function() {
+        require('../styles/test-style.css');
+    })
+}
